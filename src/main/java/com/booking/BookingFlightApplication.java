4be530f1bc430f6cd5775e404bd781bb64b32a5a
longDD_vn <long.dang@drjoy.vn>
2022-09-24 16:24:12 +0700

    add

---
+++ src/main/java/com/booking/BookingFlightApplication.java
@@ -0,0 +1,7 @@
+package com.booking;
+
+public class BookingFlightApplication {
+    public static void main(String[] args) {
+        System.out.println("booking flight app");
+    }
+}
---
+++ src/main/java/com/booking/repository/FlightRepository.java
@@ -0,0 +1,4 @@
+package com.booking.repository;
+
+public class FlightRepository {
+}
