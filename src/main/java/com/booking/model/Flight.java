13d11635bf0d277ab21c691a55bf2e25ffbbf559
longDD_vn <long.dang@drjoy.vn>
2022-09-24 16:39:29 +0700

    add

--- src/main/java/com/booking/model/Flight.java
+++ src/main/java/com/booking/model/Flight.java
@@ -1,4 +1,14 @@
 package com.booking.model;
 
+import java.util.Date;
+
 public class Flight {
+    private int flightId;
+    private String flightCode;
+    private String fightName;
+    private String fromPlace;
+    private String toPlace;
+    private Date startTimePlan;
+    private Date endTimePlan;
+    private Date totalSeat;
 }
