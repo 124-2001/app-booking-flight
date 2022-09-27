5d40fa7f8034b3309f2c3972621e1d039aca15c2
124-2001 <buinam1242001@gmail.com>
2022-09-27 09:02:03 +0700

    ok

--- src/main/java/com/booking/BookingFlightApplication.java
+++ src/main/java/com/booking/BookingFlightApplication.java
@@ -2,12 +2,9 @@ package com.booking;
 
 import com.booking.View.MenuMain;
 
-import java.util.Random;
-
 public class BookingFlightApplication {
     public static void main(String[] args) {
         MenuMain main = new MenuMain();
         main.DisplayMain();
-
     }
 }
--- src/main/java/com/booking/View/ViewForAdmin/NotificationVoucherFlight.java
+++ src/main/java/com/booking/View/ViewForAdmin/NotificationVoucherFlight.java
@@ -1,4 +1,13 @@
 package com.booking.View.ViewForAdmin;
 
+import java.security.PublicKey;
+
 public class NotificationVoucherFlight {
+    public void MakeVoucher(){
+        String str ="ABCDEFGHIYKLMNOUPQ123456789";
+        String ramdom ="";
+        for (int i=0;i<8;i++){
+
+        }
+    }
 }
