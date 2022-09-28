8a0987dd52b5ebd65da76e24fac20f2297c2fc6d
124-2001 <buinam1242001@gmail.com>
2022-09-28 11:46:33 +0700

    nam_commit
    Them doc va viet file voi chuoi json

--- src/main/java/com/booking/controller/LogicData/LogicFile.java
+++ src/main/java/com/booking/controller/LogicData/LogicFile.java
@@ -1,4 +1,56 @@
 package com.booking.controller.LogicData;
 
+import com.google.gson.Gson;
+
+import java.io.*;
+
 public class LogicFile {
+    public void WriteFile(String json,String url){
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+            File file = new File(url);
+            // if file  exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // true = append file
+            fw = new FileWriter(file.getAbsoluteFile(), true);
+            bw = new BufferedWriter(fw);
+            bw.write(json+"\n");
+            System.out.println("Done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+                if (fw != null)
+                    fw.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public void ReadFile(Object object,String url) throws FileNotFoundException {
+        Gson gson = new Gson();
+        // Đọc dữ liệu từ File với File và FileReader
+        File file = new File(url);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                object = gson.fromJson(line,Object.class);
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
+        } finally {
+            try {
+                reader.close();
+                // file.close();
+            } catch (IOException ex) {
+            }
+        }
+    }
 }
