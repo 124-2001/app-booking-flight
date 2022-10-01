4cc0f30fb00ab98343d7ea34b73d8b828897d687
Quan <98805733+quan11304@users.noreply.github.com>
2022-10-01 15:23:52 +0700

    Update DateRegex.java
    Chỉnh sửa lại DateRegex

--- src/main/java/com/booking/controller/Regex/DateRegex.java
+++ src/main/java/com/booking/controller/Regex/DateRegex.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class DateRegex {
     public static boolean dateRegex(String inputDate) {
-        String regexDate = "(^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{2,4}$) |" +
-                "(^[0-9]{1,2}\\-[0-9]{1,2}\\-[0-9]{2,4}$)";
+        String regexDate = "(^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$)|" +
+                "(^[0-9]{1,2}-[0-9]{1,2}-[0-9]{2,4}$)";
 		// accepts both dd/mm/yyyy and dd-mm-yyyy
         boolean valid = Pattern.matches(regexDate,inputDate);
         return !valid; // Match regex => false => while loop won't be activated
