246e92077559ecac0644f353e615a4dc606e79ca
Quan <98805733+quan11304@users.noreply.github.com>
2022-10-02 18:41:20 +0700

    Update Notifications.java
    (Not sure if this is correct) Thêm phần hiển thị thông báo.

--- src/main/java/com/booking/View/ViewForUser/Notifications.java
+++ src/main/java/com/booking/View/ViewForUser/Notifications.java
@@ -1,7 +1,10 @@
 package com.booking.View.ViewForUser;
 
+import static com.booking.View.ViewMain.SignIn.signedIn;
+
 public class Notifications {
     public void notifications() {
         System.out.println("*************** Thông báo **************");
+        System.out.println(signedIn.getNotification());
     }
 }
